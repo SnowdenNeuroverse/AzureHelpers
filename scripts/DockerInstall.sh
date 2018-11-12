2669934e81f1edca40f29a01d1fa8b3160b57a21
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2018-11-12 12:52:42 +0800

    Update DockerInstall.sh

--- scripts/DockerInstall.sh
+++ scripts/DockerInstall.sh
@@ -2,6 +2,8 @@ sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce=$1
+if sudo apt-key fingerprint 9DC858229FC7DD38854AE2D88D81803C0EBFCD88 | grep -q 'Docker '; then
+   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+   sudo apt-get update
+   sudo apt-get install -y docker-ce=$1
+fi
